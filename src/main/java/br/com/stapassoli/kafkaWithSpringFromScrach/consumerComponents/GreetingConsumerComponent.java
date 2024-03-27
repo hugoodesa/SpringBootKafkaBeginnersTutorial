ed302d0130003873d1edd815495d1c399720139e
hugoodesa <hugoodesa@gmail.com>
2024-03-27 01:17:10 -0300

    using headers

--- src/main/java/br/com/stapassoli/kafkaWithSpringFromScrach/consumerComponents/GreetingConsumerComponent.java
+++ src/main/java/br/com/stapassoli/kafkaWithSpringFromScrach/consumerComponents/GreetingConsumerComponent.java
@@ -3,6 +3,8 @@ package br.com.stapassoli.kafkaWithSpringFromScrach.consumerComponents;
 import br.com.stapassoli.kafkaWithSpringFromScrach.messageConverter.Greeting;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,8 +12,9 @@ import org.springframework.stereotype.Component;
 public class GreetingConsumerComponent {
 
     @KafkaListener(id = "greeting", groupId = "greetingTopic", containerFactory = "greetingKafkaListenerContainerFactory", topics = "greetingTopic")
-    public void readGreeting(Greeting greeting) {
+    public void readGreeting(Greeting greeting, @Header(KafkaHeaders.RECEIVED_PARTITION) String id) {
         System.out.println("===== Greeting =====");
+        System.out.println("partition id : " + id);
         System.out.println(greeting);
         System.out.println("====================");
     }
